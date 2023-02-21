1e396556ed5f66a5597f95c141dd037c9383f00d
Kevin <kevin@rocher-dev.com>
2023-02-21 12:19:05 +0100

    v 0.3.0
    - Bypass protection for static content.

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 cat <<EOF
 
 ###################################
-## Quick htpasswd v 0.2.0
+## Quick htpasswd v 0.3.0
 ###################################
 
 EOF
@@ -30,6 +30,7 @@ while [[ "${_HTPASSWD_USERNAME}" = "" ]]; do
     read -p "What’s your username ? " _HTPASSWD_USERNAME;
 done
 htpasswd -c  "${_PATH}/.htpasswd" "${_HTPASSWD_USERNAME}";
+read -p "Do you need to bypass protection for static assets ? Type yes/[no] :" _HTACCESS_STATIC_BYPASS;
 
 ###################################
 ## Generate htaccess
@@ -45,10 +46,24 @@ Require valid-user
 EOF
 );
 
+# Bypass protection for admin-ajax.php
 if [[ -f "admin-ajax.php" ]];then
     _HTACCESS_CONTENT=$(cat <<EOF
 ${_HTACCESS_CONTENT}
 <Files admin-ajax.php>
+  Order allow,deny
+  Allow from all
+  Satisfy any
+</Files>
+EOF
+);
+fi;
+
+# Bypass protection for static content
+if [[ "${_HTACCESS_STATIC_BYPASS}" == 'yes' || "${_HTACCESS_STATIC_BYPASS}" == 'y' ]];then
+    _HTACCESS_CONTENT=$(cat <<EOF
+${_HTACCESS_CONTENT}
+<Files ~ "\.(css|js|jpg|png|svg|jpeg|gif)$">
   Order allow,deny
   Allow from all
   Satisfy any
